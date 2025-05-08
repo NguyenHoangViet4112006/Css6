059e28ad8cc96480e09a3c15cb0f5eb32bd752d5
NguyenHoangViet4112006 <viet4112006@gmail.com>
2025-05-08 07:57:55 +0700

    Add files via upload

---
+++ b7ss6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("nhap vao mot so nguyen ");
+    scanf("%d", &n);
+
+    printf("cac uoc cua %d la ", n);
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+    return 0;
+}
